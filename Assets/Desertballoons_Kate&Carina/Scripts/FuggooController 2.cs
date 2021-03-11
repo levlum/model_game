62189b9072cf195d06f7aadc1832506a61abc69d
CarinaSt <carina.steindl@edu.fh-joanneum.at>
2021-03-11 14:31:25 +0100

    deleting useless stuff part2

---
+++ Assets/Desertballoons_Kate&Carina/Scripts/FuggooController 2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuggooController : MonoBehaviour
+{
+ public GameObject explosion; // drag your explosion prefab here
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    void OnCollisionEnter()
+    {
+        GameObject expl = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
+        Destroy(gameObject); // destroy the grenade
+        Destroy(expl, 3); // delete the explosion after 3 seconds
+    }
+}
+}
