67cd6ea0de09e3d61c27f9c819c426280f703273
armaghanzhand <armaghanzhand@gmail.com>
2021-03-15 21:54:58 +0100

    platforms+rings
    Added 2 platforms and 2 rings/
    +problem : Quad didn't worked,

--- Assets/Armaghan-and-Janina/Scripts/Camera_8.cs
+++ Assets/Armaghan-and-Janina/Scripts/Camera_8.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-public class Camera_ray : MonoBehaviour
+public class Camera_8 : MonoBehaviour
 {
-    public int _layermask;
+     public int _layermask;
     public Vector3 _direction;
-    public 
+    public ForceMode _forcemode;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +20,9 @@ public class Camera_ray : MonoBehaviour
     {
         RaycastHit hit;
         var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, 50) && hit.rigidbody != null)
+        if (Physics.Raycast(ray, out hit, _layermask) && hit.rigidbody != null)
         {
-            hit.rigidbody.AddForce(new Vector3(0,0.1f,0), ForceMode.Impulse);
+            hit.rigidbody.AddForce(_direction, _forcemode);
         }
     }
 }
