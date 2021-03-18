ccc5daf92c0bd2d2b4f97f3cbb2e9a86c8c65614
lotsa11 <lisafresser@gmx.de>
2021-03-18 21:18:21 +0100

    Change size of basket

--- Assets/Tina-Lisa/scripts/cars.cs
+++ Assets/Tina-Lisa/scripts/cars.cs
@@ -10,20 +10,34 @@ namespace Tisa
     {
         public Text coinText;
         public GameObject korb;
-       
+        public int gross = 0; 
         public int coinCount = 0;
+        public GameObject korbrand;
+        public Collider other;
 
         //public GameManager gameManager;
         // Start is called before the first frame update
         void Start()
         {
-
+            korb = GameObject.FindGameObjectWithTag("korb");
+            korbrand = GameObject.FindGameObjectWithTag("korbrand");
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            if (gross > 0){
+                 korb.transform.localScale = new Vector3(5, 1, 6);
+                  korb.transform.position = new Vector3(-5, 5, 0);
+                  korbrand.transform.localScale = new Vector3(1/2.0F, 1/3.0F, 5);
+                    other.gameObject.SetActive(false);
+                    gross --;
+                    }
+                    else {
+                         korb.transform.localScale = new Vector3(2, 0, 2);
+                  korb.transform.position = new Vector3(-10, 5, 0);
+                  korbrand.transform.localScale = new Vector3(1/10.0F, 1/8.0F, 1*1.4F);
+                    }
         }
 
         private void FixedUpdate()
@@ -34,14 +48,18 @@ namespace Tisa
         private void OnTriggerEnter(Collider other)
 
         {
+            this.other = other;
             if (other.gameObject.CompareTag("car"))
             {
-                other.gameObject.SetActive(false);
+                    gross = 100;
+               
                 //gameManager.addPoint();
                 // korb.transform.position = new Vector3(0, 1.5f, 0);
                 coinCount++;
-                UnityEngine.Debug.Log("car im korb");
+               // UnityEngine.Debug.Log(myFirstObjectReference);
             }
         }
+
+    
     }
 }
