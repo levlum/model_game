6b9c73ad7de62ef6d029c95e6aafa707cd2d98dd
CarinaSt <carina.steindl@edu.fh-joanneum.at>
2021-03-18 12:29:23 +0100

    implemented other repository

--- Assets/Desertballoons_Kate&Carina/Scripts/MainCameraController.cs
+++ Assets/Desertballoons_Kate&Carina/Scripts/MainCameraController.cs
@@ -2,24 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Kate_Carina {
-    public class MainCameraController : MonoBehaviour
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
-            RaycastHit hit;     //variable
-            var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition); // we create a ray
-            if(Physics.Raycast(ray, out hit, 50) && (hit.rigidbody != null));       //put it into information with physics and give information
-                if(hit.collider.tag == "jumpy") {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;     //variable
+        var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition); // we create a ray
+        if (Physics.Raycast(ray, out hit, 50) && (hit.rigidbody != null))
+        {       //put it into information with physics and give information
+            if (hit.collider.tag == "jumpy")
+            {
                 hit.rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
-            } 
+            }
         }
     }
 }
