c961c51fadde1b7aa299e5dee9c5b15f540e993e
LeaSuLinHabel <lea.habel@yahoo.de>
2021-03-18 16:21:18 +0100

    rebuild

--- Assets/#7_Lukas_Lin/Scripts/funnel_controller.cs
+++ Assets/#7_Lukas_Lin/Scripts/funnel_controller.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.InputManager;
 
 public class funnel_controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject[] funnel;
     void Start()
     {
-        
+        funnel = GameObject.FindGameObjectsWithTag("funnel");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            funnel[0].transform.position += new Vector3(1f, 0,0);
+        } 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            funnel[0].transform.position += new Vector3(-1f, 0,0);
+        } 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            funnel[0].transform.position += new Vector3(0, 0,1f);
+        } 
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            funnel[0].transform.position += new Vector3(0, 0,-1f);
+        }
+
+        if (funnel[0].transform.position.x >= 25f)
+        {
+            
+        } 
     }
+    
 }
