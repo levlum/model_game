b2af8b0d5941dda2b57d8098f580ba564c1ad2b6
Lev Ledit <lev@gamegestalt.com>
2021-03-19 14:59:21 +0100

    moving platforms

--- Assets/Yasi_Caro/scripts/camera_ray.cs
+++ Assets/Yasi_Caro/scripts/camera_ray.cs
@@ -7,7 +7,8 @@ namespace yasi_caro {
     {
         public LayerMask ignoreMe;
         public Transform crosshair_t;
-        public Transform push_object;
+        public float power = 10f;
+        // public Transform push_object;
 
         // Start is called before the first frame update
         void Start()
@@ -20,26 +21,14 @@ namespace yasi_caro {
         {
             RaycastHit hit;
             var ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-            crosshair_t.position = new Vector3( crosshair_t.position.x, 0f, crosshair_t.position.z); //below floor
-            if (!Input.GetButton("Fire1")){
-                push_object.GetComponent<Rigidbody>().isKinematic = true; //make push object not physical
-                push_object.position = new Vector3( push_object.position.x , -1f, push_object.position.z);
-            }
-            if (Physics.Raycast(ray, out hit, 50f, ~ignoreMe)) {
+            
+            if (Physics.Raycast(ray, out hit, 500f, ~ignoreMe)) {
                 if (hit.rigidbody != null) {
-                    hit.rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
+                    var direction = hit.point - hit.transform.position;
+                    hit.rigidbody.AddForce(direction * Time.deltaTime * power);
                 }
-                else if (hit.collider.name == "Floor"){
-                    crosshair_t.position = new Vector3( hit.point.x , 0.51f, hit.point.z); //above floor
-                    if (Input.GetButton("Fire1")){
-                        if (push_object.GetComponent<Rigidbody>().isKinematic){
-                            push_object.position = new Vector3( hit.point.x , Mathf.Max(-1f,push_object.position.y), hit.point.z);
-                            push_object.GetComponent<Rigidbody>().isKinematic = false;
-
-                        }
-                        push_object.GetComponent<Rigidbody>().AddForce(Vector3.up * 100f * Time.deltaTime, ForceMode.Impulse);
-                    }
-                }
+                
+                Debug.Log("mouse ray collission.");
             } 
         }
     }
