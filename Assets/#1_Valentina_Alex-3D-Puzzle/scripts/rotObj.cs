8601121e3b430177d1c93eb93902ab2476ed3334
Alex Moser <73650025+Grado0815@users.noreply.github.com>
2021-03-19 12:58:29 +0100

    LVL3

--- Assets/#1_Valentina_Alex-3D-Puzzle/scripts/rotObj.cs
+++ Assets/#1_Valentina_Alex-3D-Puzzle/scripts/rotObj.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class rotObj : MonoBehaviour
 {
-    private float rotSpeed = 20;
+    private float rotSpeed = 5;
 
     private void OnMouseDrag()
     {
         float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
         float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
         
-        transform.RotateAround(Vector3.up, -rotX);
-        transform.RotateAround(Vector3.right, rotX);
+        transform.RotateAround(Vector3.up, -rotY);
+        transform.RotateAround(Vector3.left, rotX);
     }
 }
