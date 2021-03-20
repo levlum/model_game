bfc53982e3920195c4fefdab2c96e7a7e2dc9d18
lotsa11 <lisafresser@gmx.de>
2021-03-20 21:18:27 +0100

    Throw machanik

---
+++ Assets/Tina-Lisa/scripts/Projecttile.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projecttile : MonoBehaviour
+{
+    public Rigidbody bulletPrefabs;
+    public GameObject cursor;
+    public LayerMask layer;
+    public Transform shootPoint;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       LaunchProjectile();
+    }
+    void LaunchProjectile()
+    {
+        Ray camRay = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(camRay, out hit, 100f, layer))
+        {
+            cursor.SetActive(true);
+            cursor.transform.position = hit.point + Vector3.up * 0.1f;
+
+            Vector3 Vo = CalculateVelocity(hit.point, shootPoint.position, 1f);
+            transform.rotation = Quaternion.LookRotation(Vo);
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                Rigidbody obj = Instantiate(bulletPrefabs, shootPoint.position, Quaternion.identity);
+                obj.velocity = Vo;
+            }
+        }
+        else{
+            cursor.SetActive(false);
+        }
+    }
+
+    Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float time)
+    {
+            //define distance x and y
+            Vector3 distance = target - origin;
+            Vector3 distanceXZ = distance;
+            distanceXZ.y = 0f;
+
+            //create float that represents our distance
+            float Sy = distance.y;
+            float Sxz = distanceXZ.magnitude;
+
+            float Vxz = Sxz / time;
+            float Vy = Sy / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
+
+
+            Vector3 result = distanceXZ.normalized;
+            result *= Vxz;
+            result.y = Vy;
+
+            return result;
+    }
+}
