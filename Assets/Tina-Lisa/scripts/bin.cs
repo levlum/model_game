72d388a3b390f57acee1a0602682e5661e7fa21c
lotsa11 <lisafresser@gmx.de>
2021-03-24 21:03:41 +0100

    DifferenzierungGlasDose

---
+++ Assets/Tina-Lisa/scripts/bin.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bin : MonoBehaviour
+{
+    public Text binText;
+    public int binCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   private void FixedUpdate()
+        {
+            binText.text = binCount.ToString();
+        }
+        private void OnTriggerEnter(Collider other)
+
+        {
+            if (other.gameObject.CompareTag("can"))
+            {
+                other.gameObject.SetActive(false);
+                binCount++;
+               UnityEngine.Debug.Log("can in bin");
+            }
+        }
+}
