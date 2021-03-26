b093fb5778c68232193f9421c44d64fcea2e1ce8
lotsa11 <lisafresser@gmx.de>
2021-03-26 11:34:13 +0100

    Points fixed

--- Assets/Tina-Lisa/scripts/bin.cs
+++ Assets/Tina-Lisa/scripts/bin.cs
@@ -7,35 +7,42 @@ public class bin : MonoBehaviour
 {
     public Text binText;
     public int binCount = 0;
+    private static float                  points; 
+    public points script;
+    public float _point;
     // Start is called before the first frame update
     void Start()
     {
     
-    
+    script = GameObject.FindObjectOfType<points>();
+     _point = script._points;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        points = script._points;  //  Update our score continuously.
+        binText.text = script._points.ToString();
     }
    private void FixedUpdate()
         {
-            binText.text = binCount.ToString();
+       // binText.text = points.ToString();
         }
         private void OnTriggerEnter(Collider other)
 
         {
+            script = GameObject.FindObjectOfType<points>();
             if (other.gameObject.CompareTag("can"))
             {
                 other.gameObject.SetActive(false);
-                binCount++;
-               UnityEngine.Debug.Log("can in bin");
+                script._points+=1;
+               UnityEngine.Debug.Log("Points: " + script._points);
             }
             if (other.gameObject.CompareTag("bottle"))
             {
-                binCount--;
-               UnityEngine.Debug.Log("can in bin");
+                script._points--;
+               UnityEngine.Debug.Log("Points: " + script._points);
             }
         }
 }
