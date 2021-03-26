ac77d4a673693369cb848d9346ca41901e41b4ab
Lev Ledit <lev@gamegestalt.com>
2021-03-26 16:35:10 +0100

    camera follow script changed a bit

--- Assets/Yasi_Caro/scripts/DampCamera2D.cs
+++ Assets/Yasi_Caro/scripts/DampCamera2D.cs
@@ -14,7 +14,7 @@ using UnityEngine;
     void Update()
     {
         // Define a target position above and behind the target transform
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+        Vector3 targetPosition = target.position + new Vector3(0, 10, -70);
 
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
