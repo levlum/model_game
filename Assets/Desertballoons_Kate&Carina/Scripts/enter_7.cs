457fc3255c602396bf89e724278ad2010cfe032c
katherinepalishchuk <56172141+katherinepalishchuk@users.noreply.github.com>
2021-04-06 16:37:37 +0300

    walls, counting scores, winner menu

---
+++ Assets/Desertballoons_Kate&Carina/Scripts/enter_7.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enter_7 : MonoBehaviour
+{
+    public int coins_7 = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "jumpy")
+        {
+            coins_7++;
+        }
+    }
+}
