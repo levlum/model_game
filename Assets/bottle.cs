da807e2713671e6ef4578ac82ba42ec6934ac18d
lotsa11 <49392443+lotsa11@users.noreply.github.com>
2021-12-11 18:50:39 +0100

    small changes

---
+++ Assets/bottle.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bottle : MonoBehaviour
+{
+
+    public static float point;
+    public float _point;
+    //public points script;
+    //public Slider slider;
+   // public int randomPlant;
+public AudioSource plantAudio;
+ public ParticleSystem drops;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // script = GameObject.FindObjectOfType<points>();
+              // _point = script._points;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // _point = script._points;  //  Update our score continuously.
+
+    }
+
+         //public Transform explosionPrefab;
+                void OnCollisionEnter(Collision collision) {
+                   if (collision.gameObject.tag == "house")
+                   Debug.Log("braune Flasche an Wand");
+                   {//AudioSource.PlayClipAtPoint(plantAudio, this.gameObject.transform.position);
+                      plantAudio.Play();
+                      //script._points+=3;
+                      //slider.value = script._points;
+                      //this.gameObject.SetActive(false) ;
+                         ContactPoint contact = collision.contacts[0];
+                         Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+                        Vector3 pos = contact.point;
+                        drops.Play();
+                       // Instantiate(explosionPrefab, pos, rot);
+
+                       Destroy(this.gameObject, 0.1f);
+
+
+                   }
+                }
+}
